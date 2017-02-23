5391702538e8d6e86adf8593af5f104aae2ccfad
isho <idoshoshani@gmail.com>
2017-02-23 14:02:55 -0800

    demo added

---
+++ IdoBlog/resources/views/demo.blade.php
@@ -0,0 +1,87 @@
+
+<html>
+
+    <head>
+
+        <title>AR Demo</title>
+        <link rel="stylesheet" href="css/mobileDesign.css">
+    </head>
+    <body>
+        <div class="mainWrapper">
+            <header id="link-area">
+
+                <div id="profile-symbol">
+
+                    <svg id="parent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.76 63.14"><defs></defs><title>Icon1</title><g id="Layer_3" data-name="Layer 3">
+                    <path class="cls-1" d="M33.19,31a16.34,16.34,0,1,0-14.73-.23C7.81,33.37,0,41.66,0,51.51a18.43,18.43,0,0,0,.27,3.12c5.87,5.17,15.15,8.52,25.61,8.52,9.78,0,18.53-2.93,24.42-7.54a18.41,18.41,0,0,0,.47-4.1C50.76,41.94,43.39,33.84,33.19,31Z"/>
+                </g>
+                </svg>
+
+					<div id="popup" class="hidden popupHeader">
+						<section class="headerWrapper">
+            	    			<h2>Bub Cat</h2>
+								<svg id="closeButton" class="close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+									<path d="M17.459,16.014l8.239-8.194c0.395-0.391,0.395-1.024,0-1.414c-0.394-0.391-1.034-0.391-1.428,0  l-8.232,8.187L7.73,6.284c-0.394-0.395-1.034-0.395-1.428,0c-0.394,0.396-0.394,1.037,0,1.432l8.302,8.303l-8.332,8.286  c-0.394,0.391-0.394,1.024,0,1.414c0.394,0.391,1.034,0.391,1.428,0l8.325-8.279l8.275,8.276c0.394,0.395,1.034,0.395,1.428,0  c0.394-0.396,0.394-1.037,0-1.432L17.459,16.014z" fill="#121313" id="Close"/>
+								</svg>
+        				</section>
+
+        				<article  class="profileBody">
+            				<img src="http://i.imgur.com/y5jjMX5.jpg">
+            				<p>
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                				Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.
+    	        			</p>
+							<div class="points">
+								<div class="starWrapper">
+									<svg class="stars" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 475.24 193.05"><title>stars</title>
+										<path class="star-1" d="M260.76,40.79S239.23,0,237.62,0h0C236,0,214.48,40.79,214.48,40.79s-49,8.22-49,11.62h0a.76.76,0,0,0,.06.25c1.23,4.43,32.93,36.18,32.93,36.18s-9.26,44.81-7.59,46a.2.2,0,0,0,.11,0,5,5,0,0,0,1.81-.79l.48-.28,1.09-.68.61-.39c2.69-1.76,42-21.67,42.63-22h0c1.2.61,39.95,20.25,42.62,22l.61.39,1.09.68.48.28a5,5,0,0,0,1.81.79.2.2,0,0,0,.11,0c1.68-1.22-7.59-46-7.59-46s31.7-31.75,32.93-36.18a.76.76,0,0,0,.06-.25h0C309.77,49,260.76,40.79,260.76,40.79Z"/>
+										<path class="star-1" d="M95.28,98.94S73.75,58.15,72.15,58.15h0C70.53,58.15,49,98.94,49,98.94S0,107.17,0,110.56H0a.76.76,0,0,0,.06.25C1.29,115.24,33,147,33,147s-9.26,44.81-7.59,46a.2.2,0,0,0,.11,0,5,5,0,0,0,1.81-.79l.48-.28,1.09-.68.61-.39c2.69-1.76,42-21.67,42.63-22h0c1.2.61,39.95,20.25,42.62,22l.61.39,1.09.68.48.28a5,5,0,0,0,1.81.79.2.2,0,0,0,.11,0c1.68-1.22-7.59-46-7.59-46s31.7-31.75,32.93-36.18a.76.76,0,0,0,.06-.25h0C144.29,107.17,95.28,98.94,95.28,98.94Z"/>
+										<path class="star-1" d="M380,98.94s21.53-40.79,23.14-40.79h0c1.61,0,23.14,40.79,23.14,40.79s49,8.22,49,11.62h0a.76.76,0,0,1-.06.25C474,115.24,442.25,147,442.25,147s9.26,44.81,7.59,46a.2.2,0,0,1-.11,0,5,5,0,0,1-1.81-.79l-.48-.28-1.09-.68-.61-.39c-2.69-1.76-42-21.67-42.63-22h0c-1.2.61-39.95,20.25-42.62,22l-.61.39-1.09.68-.48.28a5,5,0,0,1-1.81.79.2.2,0,0,1-.11,0c-1.68-1.22,7.59-46,7.59-46s-31.7-31.75-32.93-36.18a.76.76,0,0,1-.06-.25h0C330.95,107.17,380,98.94,380,98.94Z"/>
+									</svg>
+								</div>
+								<div id="counter" class="number">0</div>
+							</div>
+        				</article>
+					</div>
+
+					<div id="popupTag" class="hidden popupHeader popupTag">
+						<section class="headerWrapper">
+							<div>
+                				<img class="smallProfilePic" src="http://i.imgur.com/y5jjMX5.jpg">
+							</div>
+								<svg id="tagClose" class="close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+									<path d="M17.459,16.014l8.239-8.194c0.395-0.391,0.395-1.024,0-1.414c-0.394-0.391-1.034-0.391-1.428,0  l-8.232,8.187L7.73,6.284c-0.394-0.395-1.034-0.395-1.428,0c-0.394,0.396-0.394,1.037,0,1.432l8.302,8.303l-8.332,8.286  c-0.394,0.391-0.394,1.024,0,1.414c0.394,0.391,1.034,0.391,1.428,0l8.325-8.279l8.275,8.276c0.394,0.395,1.034,0.395,1.428,0  c0.394-0.396,0.394-1.037,0-1.432L17.459,16.014z" fill="#121313" id="Close"/>
+								</svg>
+        				</section>
+
+        				<article  class="tagBody">
+            				<img alt="Cars" src="https://visualhunt.com/photos/m/1/blurred-motion-of-cars-on-street.jpg">
+            				<p>
+								Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                				Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.
+    	        			</p>
+        				</article>
+					</div>
+
+                </div>
+
+            </header>
+
+            <main id="stream-area">
+            </main>
+        </div>
+
+        <footer id="tag-area">
+
+
+        </footer>
+
+        <video id="video" autoplay style="display: none"></video>
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/mobileDesign.js"></script>
+    <script src="js/lib/three.min.js"></script>
+    <script src="js/Webgl.js"></script>
+    </body>
+</html>
+<body>
